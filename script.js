7f0cd8d4518ff5f41985712aa6adc5e03ec8e2dd
Andrew Ingraham <aingraham@flatiron.com>
2020-10-03 18:21:17 -0400

    Add navigation to view previous days

--- script.js
+++ script.js
@@ -1,75 +1,54 @@
+const $content = $('#content');
+const $prev = $('#prev');
+const $next = $('#next');
+const firstDate = new Date('Sep 7 2020');
+const currentDate = new Date();
+const currentDayNum = date_diff_indays(firstDate, currentDate);
+let dayNum = currentDayNum;
+const numToShow = 6;
+
+
+setContent(dayNum);
+
+$prev.click(() => {
+  if (dayNum <= 0) {
+    return;
+  }
+  dayNum--;
+  setContent(dayNum);
+});
+
+$($next).click(() => {
+  if (dayNum >= currentDayNum) {
+    return;
+  }
+  dayNum++;
+  setContent(dayNum);
+});
+
 function date_diff_indays(date1, date2) {
   dt1 = new Date(date1);
   dt2 = new Date(date2);
   return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
 }
 
-const firstDate = new Date('Sep 7 2020');
-const currentDate = new Date();
-const dayNum = date_diff_indays(firstDate, currentDate);
+function setContent(dayNum) {
+  const startIndex = dayNum * numToShow;
 
-const numToShow = 6;
-const startIndex = dayNum * numToShow;
-const $content = $('#content');
-
-for(let i = startIndex; i < startIndex + numToShow; i++) {
-  $content.append(tiktoks[i]);
-}
-
-/*
-setInterval(() => {
-  console.log('in setInterval');
-  const $iframes = $('iframe');
-  $iframes.each((index, element) => {
-    console.log('this', element);
-    var cssLink = document.createElement("link");
-    cssLink.href = "./tiktokstyles.css"; 
-    cssLink.rel = "stylesheet"; 
-    cssLink.type = "text/css"; 
-    element.document.head.appendChild(cssLink);
-  });
-}, 5000);
-*/
-
-/*
-function render(picNum) {
-	return `<img src="./img/${picNum}.jpg">`;
-}
-
-const picCount = 9;
-const picNums = randomize(Array(picCount).fill().map((_, i) => i + 1));
-console.log(picNums);
-
-const fadeLength = 1000;
-const messageLength = 10000;
-
-const $content = $('#content');
-
-let index = 0;
-const item = render(picNums[index]);
-$content.hide();
-$content.html(item);
-$content.fadeIn(fadeLength);
-
-setInterval(function() {
-	index = (index + 1) % picNums.length;
-	const item = render(picNums[index]);
-	$content.fadeOut(fadeLength, function() {
-		$content.html(item);
-	}).fadeIn(fadeLength);
-}, messageLength);
-
-function swap(arr, i, j) {
-  const tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-}
-
-function randomize(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    const randomIndex = Math.floor(Math.random() * (arr.length - i)) + i;
-    swap(arr, i, randomIndex);
+  if (dayNum <= 0) {
+    $prev.prop('disabled', true);
+  } else {
+    $prev.prop('disabled', false);
   }
-  return arr;
+
+  if (dayNum >= currentDayNum) {
+    $next.prop('disabled', true);
+  } else {
+    $next.prop('disabled', false);
+  }
+
+  $content.empty();
+  for(let i = startIndex; i < startIndex + numToShow; i++) {
+    $content.append(tiktoks[i]);
+  }
 }
-*/
